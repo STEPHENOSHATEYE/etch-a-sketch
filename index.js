085526107c516f37b4da955279b953211b441d08
STEPHENOSHATEYE <155460166+STEPHENOSHATEYE@users.noreply.github.com>
2024-06-08 21:50:49 +0100

    created function to customize the amount of grids to be generated in index.js

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
+//container to store all grids;
 const gridContainer = document.querySelector('.grids');
+//button object for customizing number of grids;
+const setGridSizeButton = document.querySelector('.grid-size');
 
-//square has equal size, so gird is 500x500
+//square has equal size, so gird is 500x500;
 const sizeOfGridContainer = 500;
-//The amount of squares to be appended in the grids container
-let numberOfGrids = 64;
-//The total amount of equal squares in the grid container
+//The amount of squares to be appended in the grids container;
+let numberOfGrids = 2;
+//The total amount of equal squares in the grid container;
 const squareOfNumberOfGrids = numberOfGrids**2;
 
-//for every iteration between 0 and squareOfNumberOfGrids, 
-for (let i = 0; i < (squareOfNumberOfGrids); i++){
-    const gridChild = document.createElement('div');
-    gridChild.className = 'grid-child';
-    gridChild.style.width = `${((sizeOfGridContainer/numberOfGrids)-2)}px`
-    gridChild.style.backgroundColor = 'white';
-    gridContainer.appendChild(gridChild);
-};
-
-//add event listener to all squares in the grid container
-const gridChild = document.querySelectorAll('.grid-child');
-gridChild.forEach((grid)=>{
-    grid.addEventListener('mouseover',() =>{
-        grid.style.backgroundColor = 'blue';
+//set evente listner to active funtion when button is clicked;
+setGridSizeButton.addEventListener('click', () => {
+    const gridChildren = document.querySelectorAll('.grid-child');
+    //The custom amount of squares to be appended in the grids container;
+    let customNumberOfGrids = parseInt(prompt('Enter a number from 1-64: '));
+    //The custom total amount of equal squares in the grid container;
+    const squareOfCustomNumberOfGrids = customNumberOfGrids ** 2;
+    //removes every grid in gridContainer and makes it empty;
+    gridChildren.forEach((child) =>{
+        gridContainer.removeChild(child);
     });
-})
+
+    //generates new grids in the grid container;
+    generateGrid(squareOfCustomNumberOfGrids,customNumberOfGrids);
+});
+
+
+function generateGrid(squareOfNumberOfGrids,numberOfGrids){
+    //for every iteration between 0 and squareOfNumberOfGrids;
+    for (let i = 0; i < (squareOfNumberOfGrids); i++){
+        const gridChild = document.createElement('div');
+        gridChild.className = 'grid-child';
+        gridChild.style.width = `${((sizeOfGridContainer/numberOfGrids)-2)}px`
+        gridChild.style.backgroundColor = 'white';
+        gridContainer.appendChild(gridChild);
+        debugger;
+    };
+    //add event listener to all squares in the grid container
+    const gridChild = document.querySelectorAll('.grid-child');
+    gridChild.forEach((grid)=>{
+        grid.addEventListener('mouseover',() =>{
+            grid.style.backgroundColor = 'blue';
+        });
+    })
+}
+
+generateGrid(squareOfNumberOfGrids,numberOfGrids);
 
 
 
